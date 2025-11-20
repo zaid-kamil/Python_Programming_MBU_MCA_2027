ebb3e6ed4196a22b4e14a95a635d1e9cdda58f1b
zaid kamil <xaidmetamorphos@gmail.com>
2025-11-20 11:23:54 +0530

    Add Flask application with routing and templates for home, news, and blog pages

---
+++ website7/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
